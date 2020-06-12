ade2514118fc566062bd6e3e2c275e640b27eca0
Emanuel Paulo Chambal <chambaljunior@outlook.pt>
2020-06-12 13:34:53 -0300

    Composer and phpunit without bugs

---
+++ controladora/Pages.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace controladora;
+
+class Pages{
+    public function render(){
+        return 'Ola PHP Unit';
+    }
+
+    public function returnTrue(){
+        return true;
+    }
+}
